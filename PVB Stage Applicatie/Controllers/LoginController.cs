2a437d978434e62ac26272fb1ac8d92646b823d4
MgBag <waanders.b@gmail.com>
2014-10-24 00:52:43 +0200

    Login toegevoegd en werkend. Man 3*1080P werkt zó veel fijner dan 1366*786. Man man man man

---
+++ PVB Stage Applicatie/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using PVB_Stage_Applicatie.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PVB_Stage_Applicatie.Controllers
+{
+    public class LoginController : Controller
+    {
+        private StageApplicatieEntities db = new StageApplicatieEntities();
+
+        //
+        // GET: /Login/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginForm InLogForm)
+        {
+            var persoonsgegevens = db.sp_Login(InLogForm.Gebruikersnaam, InLogForm.Wachtwoord).ToList();
+
+            if (persoonsgegevens.Count == 1)
+            {
+                return View("Jawel, ingelogd. Heuj");
+            }
+
+
+            return View(InLogForm);
+        }
+
+    }
+}
---
+++ PVB Stage Applicatie/Models/LoginForm.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PVB_Stage_Applicatie.Models
+{
+    public class LoginForm
+    {
+        public string Gebruikersnaam { get; set; }
+        public string Wachtwoord { get; set; }
+    }
+}
