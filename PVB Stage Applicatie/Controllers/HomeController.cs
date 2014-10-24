6b59dd64fc2af47b6aec8e494f759d2e19a2c0ec
krissl94 <krisscholte@hotmail.com>
2014-10-24 14:43:25 +0200

    Logingebeuren

--- PVB Stage Applicatie/Controllers/HomeController.cs
+++ PVB Stage Applicatie/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace PVB_Stage_Applicatie.Controllers
 {
@@ -13,8 +14,17 @@ namespace PVB_Stage_Applicatie.Controllers
 
         public ActionResult Index()
         {
+            MenuSelector();
+
             return View();
         }
+        public ActionResult MenuSelector()
+        {
+            RolePrincipal rp = (RolePrincipal)User;
+            var rol = rp.GetRoles();
+            return View("/Menu/AdminMenuPartial.cshtml");
+        }
+
 
     }
 }
