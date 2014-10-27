e3b0298061b6689678c8c7d3c476e428cdf5b353
MgBag <waanders.b@gmail.com>
2014-10-27 10:49:04 +0100

    Hernamingen Persoonsgegevens ontdaan.

--- PVB Stage Applicatie/Models/Rollen.cs
+++ PVB Stage Applicatie/Models/Rollen.cs
@@ -7,12 +7,14 @@ namespace PVB_Stage_Applicatie.Models
 {
     public static class Rollen
     {
-
         public const string Beheerder = "1";
         public const string Docent = "2";
         public const string Begeleider = "3";
         public const string Student = "4";
 
-
+        public const int BeheerderI = 1;
+        public const int DocentI = 2;
+        public const int BegeleiderI = 3;
+        public const int StudentI = 4;
     }
 }
