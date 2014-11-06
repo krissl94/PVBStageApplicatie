6c90ba5f6ffe4b7a71770cc8f104dabd69a44f8e
krissl94 <krisscholte@hotmail.com>
2014-11-06 21:20:16 +0100

    dingen doen

---
+++ PVB Stage Applicatie/Models/beperkInvoer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PVB_Stage_Applicatie.Models
+{
+    public class beperkInvoer : ValidationAttribute
+    {
+        private readonly string _chars;
+
+        public beperkInvoer(string chars)
+            : base("{0} contains invalid character.")
+        {
+            _chars = chars;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+    {
+
+        if (value != null)
+        {
+            for (int i = 0; i < _chars.Length; i++)
+            {
+                var valueAsString = value.ToString();
+                if (valueAsString.Contains(_chars[i]))
+                {
+                    var errorMessage = FormatErrorMessage(validationContext.DisplayName);
+                    return new ValidationResult(errorMessage);
+                }
+            }
+        }
+        return ValidationResult.Success;
+    }
+    }
+}
